19a78549f26b3e30344b940724c103e3b9f97c93
salminsimeiz <130555704+salminsimeiz@users.noreply.github.com>
2025-01-10 16:54:36 +0300

    Add files via upload

--- pythonDjango19/game_shop/task1/admin.py
+++ pythonDjango19/game_shop/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Buyer)
+class AdminBuyer(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('name', 'balance')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
+
+
+@admin.register(Game)
+class AdminGame(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
